3d2ce3f09c8a8663e839f752dd6dddfb77c1058e
test <3402502077@qq.com>
2024-05-13 00:03:58 +0800

    2.0

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
  * @Author: test 3402502077@qq.com
  * @Date: 2024-05-12 22:28:44
  * @LastEditors: test 3402502077@qq.com
- * @LastEditTime: 2024-05-12 23:55:04
+ * @LastEditTime: 2024-05-13 00:03:44
  * @FilePath: \chuankou2\src\main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <Arduino.h>
 #include <HardwareSerial.h>
-// 更多LED可以继续添加
+
 
 void setup()
 {
